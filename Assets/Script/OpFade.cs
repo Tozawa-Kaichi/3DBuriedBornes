ed7b987ecd6d367314730dcef5fb976ad2283413
戸澤海智 <st37071556@icloud.com>
2022-03-18 11:02:23 +0900

    タイトル作成

---
+++ Assets/Script/OpFade.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OpFade : MonoBehaviour
+{
+    GameObject player = default;
+    [SerializeField] float waitsecond = 2;
+    string thisScene;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        thisScene = SceneManager.GetActiveScene().name;
+    }
+    private void Update()
+    {
+        StartCoroutine(TitleBack(thisScene));
+    }
+    public void TitleLoad(string name)
+    {
+        StartCoroutine(LoadScenes(name));
+    }
+
+
+    public IEnumerator LoadScenes(string name)
+    {
+        yield return new WaitForSeconds(waitsecond);
+        yield return new WaitUntil(() => Triger.canchangescene);
+        SceneManager.LoadScene(name);
+    }
+    public IEnumerator TitleBack(string name)
+    {
+        yield return new WaitForSeconds(waitsecond);
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Assets/Script/Trigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triger : MonoBehaviour
+{
+    public static bool canchangescene = false;
+    public void Trigers()
+    {
+        canchangescene = true;
+    }
+}
